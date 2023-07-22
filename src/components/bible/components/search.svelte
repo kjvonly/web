742e921b85bfc4f6e167a3d125c367cd24ece002
kjvonly <136278437+kjvonly@users.noreply.github.com>
2023-07-22 16:23:53 -0400

    adding search results

--- src/components/bible/components/search.svelte
+++ src/components/bible/components/search.svelte
@@ -10,34 +10,49 @@
 	$: popupHeight = containerHeight - footerHeight;
 
 	let searchInput: string = '';
-
+    let selectedSuggestion = 0
 	onMount(() => {
 		let el = document.getElementById('search-popup-' + searchID);
 		let pel = el?.parentNode as HTMLElement;
 		containerHeight = pel.getBoundingClientRect().height;
 		let input = document.getElementById('search-input-' + searchID);
+        
 		input?.focus();
 		// Get the input field
 
 		input?.addEventListener('keypress', function (event) {
 			if (event.key === 'Enter') {
 				event.preventDefault();
-				dispatch('popupHandler', {
-					chapter: searchInput
-				});
+
+				// dispatch('popupHandler', {
+				// 	chapter: searchInput
+				// });
+
 			}
+
+            if (event.key === 'k'){
+                selectedSuggestion = selectedSuggestion + 1
+            }
+
 		});
 	});
 </script>
 
-<div id="search-popup-{searchID}" class="flex flex-fill w-100">
-    <div class="suggestion">
-
-    </div>
+<div id="search-popup-{searchID}" class="flex flex-fill h-100 w-100">
+	<div class="suggestion flex flex-fill h-100" style:--height={popupHeight}>
+		{#each { length: 20 } as _, i}
+			<li id="suggestion-{i+1}-{searchID}" class={i === selectedSuggestion ? 'selected' : ''}>{i + 1}</li>
+		{/each}
+	</div>
 	<div class="input w-100" bind:clientHeight={footerHeight}>
 		<div class="d-flex justify-content-start w-100">
 			<p class="m-0 text-center">Search:</p>
-			<input id="search-input-{searchID}" class="w-100" bind:value={searchInput} placeholder="thy word i have hid" />
+			<input
+				id="search-input-{searchID}"
+				class="w-100"
+				bind:value={searchInput}
+				placeholder="thy word i have hid"
+			/>
 		</div>
 	</div>
 </div>
@@ -46,7 +61,7 @@
 	.suggestion {
 		height: var(--height);
 		overflow: auto;
-        background-color: bisque;
+		background-color: bisque;
 	}
 
 	.input {
