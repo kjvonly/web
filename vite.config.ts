0664c4218f1a3fda4cd408ea4380d0591eb6fe41
kjvonly <136278437+kjvonly@users.noreply.github.com>
2023-08-05 12:10:49 -0400

    vite config

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+		}
+	},
+	server: {
+		cors: {
+			"origin": "*",
+			"methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+			"preflightContinue": true,
+			"optionsSuccessStatus": 204
+		},
+		proxy: {
+			'^/api/.*': {
+				target: 'http://{YOUR IP ADDR}:7777',
+				changeOrigin: true,
+				proxyTimeout: 6000 * 60 * 1000,
+
+			  },
+		},
+	}
+});
