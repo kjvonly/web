4948c3509d0629582cfe6cf57b9b4aa53cc21baf
man4christ <man4christ.65qa8@simplelogin.com>
2023-12-18 21:54:14 -0500

    layout tmp of navbar

--- src/theme/layout/navbar.svelte
+++ src/theme/layout/navbar.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 </script>
 
-<div class="navbar w-100">hello world</div>
+<div class="navbar w-100">
+    <div class="row w-100">
+        <div class="d-flex flex-row justify-content-between w-100">
+            <p>1</p>    
+            <p>2</p>    
+            <p>3</p>    
+        </div>
+
+    </div>
+</div>
 
 <style>
 	.navbar {
