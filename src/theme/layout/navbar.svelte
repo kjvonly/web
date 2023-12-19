4ed580d1555ffd4419282b33dc832da453e7df82
man4christ <man4christ.65qa8@simplelogin.com>
2023-12-19 00:25:07 -0500

    adds simple navbar layout

--- src/theme/layout/navbar.svelte
+++ src/theme/layout/navbar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Icon from 'svelte-awesome';
 	import bars from 'svelte-awesome/icons/bars';
+
+	import logo from '$lib/assets/cross.svg';
 </script>
 
 <div class="kjvonly-navbar w-100">
@@ -8,51 +10,69 @@
 		<div class="d-flex flex-row align-items-center justify-content-between w-100">
 			<div class="d-flex flex-row align-items-center">
 				<Icon data={bars} scale="2"></Icon>
-                <span style="font-size: 1.75rem;"></span>
-                <div class="title ms-1">KVJonly</div>
+				<div class="title"><span class="font-weight-bold">KVJ</span>only</div>
 			</div>
-            <div class="d-flex flex-row">
+            <div>
+                <img class="logo" alt="The project logo" src={logo} />
+            </div>
+			<div class="d-flex flex-row">
 				<div id="user">
 					<span> m4c </span>
-                </div>
+				</div>
 
-                <div class="d-flex align-items-center ms-1">
-                    man4christ
-                </div>
-                
-            </div>
+				<div class="d-flex align-items-center ms-1">man4christ</div>
+			</div>
 		</div>
 	</div>
 </div>
 
 <style lang="scss">
+	@import '../../scss/styles.scss';
+
 	.kjvonly-navbar {
-        height: 4.75rem;
-        padding: 1.25rem;
-        width: 100%;
-		background-color: #002868;
+		height: 4.75rem;
+		padding: 1.25rem;
+		width: 100%;
+		background-color: $base-blue;
 		color: white;
-        align-items: center;
-        display: flex;
+		align-items: center;
+		display: flex;
 	}
 
 	#user {
 		width: 3rem;
 		height: 3rem;
-        max-width: 3rem;
+		max-width: 3rem;
 		max-height: 3rem;
 
 		-webkit-border-radius: 1.5rem;
 		-moz-border-radius: 1.5rem;
 		border-radius: 1.5rem;
-		background: #BF0A30;
+		background: $base-red;
 	}
-    #user > span {
+	#user > span {
 		justify-content: center;
-        align-content: center;
-        display: flex;
-        height: 100%;
-        width: 100%;
-        flex-wrap: wrap;
-    }
+		align-content: center;
+		display: flex;
+		height: 100%;
+		width: 100%;
+		flex-wrap: wrap;
+	}
+
+	.logo {
+		fill: $base-red !important;
+		height: 3rem;
+		margin-left: 0.5rem;
+		margin-right: 0.5rem;
+	}
+
+    .title {
+        margin-left: 1rem;
+        margin-right: 1rem;
+        span {
+        font-weight: bold;
+        color: $base-red
+        }
+        font-size: 1.75rem;
+    } 
 </style>
