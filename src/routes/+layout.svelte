5dace694c9b2bca0db859e48739b1bc1f7716752
man4christ <man4christ.65qa8@simplelogin.com>
2023-12-18 20:34:13 -0500

    removes skeleton app bootstrap code from layout.svlete

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,62 +1,15 @@
 <script lang="ts">
-	// The ordering of these imports is critical to your app working properly
-
-	//import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	//import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
-	// import '../app.postcss';
-	///import { AppShell, AppRail, AppRailAnchor, AppRailTile } from '@skeletonlabs/skeleton';
-
-	import book from 'svelte-awesome/icons/book';
-	import bars from 'svelte-awesome/icons/bars';
-	import refresh from 'svelte-awesome/icons/refresh';
-	import user from 'svelte-awesome/icons/user';
-
-	import Icon from 'svelte-awesome';
 	import { onMount } from 'svelte';
 	import { keydownStore } from '../services/keydown.service';
 
 	// Import our custom CSS
 	import '../scss/styles.scss';
 
-	let currentTile: number = 0;
-
 	onMount(async () => {
 		await import('bootstrap');
 		keydownStore.add();
 	});
 </script>
-
-<!-- <AppShell>
-	<svelte:fragment slot="sidebarLeft">
-		<AppRail>
-			<svelte:fragment slot="lead">
-				<AppRailAnchor href="/">
-					<Icon data={bars} scale={3} />
-				</AppRailAnchor>
-			</svelte:fragment>
-
-			<AppRailTile href="/read" bind:group={currentTile} name="tile-1" value={0} title="tile-1">
-				<svelte:fragment slot="lead">
-					<Icon data={book} scale={3} />
-				</svelte:fragment>
-				<span>Bible</span>
-			</AppRailTile>
-			<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
-				<svelte:fragment slot="lead"><Icon data={refresh} scale={3} /></svelte:fragment>
-				<span>Memory</span>
-			</AppRailTile>
-
-			<svelte:fragment slot="trail">
-				<AppRailAnchor href="/" target="_blank" title="Account">
-					<Icon data={user} scale={3} />
-				</AppRailAnchor>
-			</svelte:fragment>
-		</AppRail>
-	</svelte:fragment> 
-	
-</AppShell>	-->
 
 <div class="layout">
 	<slot />
