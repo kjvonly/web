0e182caf9eec6eb2a7bf02a01f0a4debb52d989a
man4christ <man4christ.65qa8@simplelogin.com>
2023-12-30 12:27:58 -0500

    adds menu.scss to global scss folder

--- src/components/bible/components/nested-menu.svelte
+++ src/components/bible/components/nested-menu.svelte
@@ -15,48 +15,11 @@
 		<svelte:self bind:item={c} />
 	{/each}
 {:else if item.children && item.children.length > 0}
-	<menu title={item.title}>
+	<menu class="kjv-menu-item-parent" title={item.title}>
 		{#each item.children as nested}
 			<svelte:self bind:item={nested} />
 		{/each}
 	</menu>
 {:else}
-	<menu role="none" on:click={item.handler} title={item.title}></menu>
+	<menu class="kjv-menu-item" role="none" on:click={item.handler} title={item.title}></menu>
 {/if}
-
-<style>
-	menu {
-		position: absolute;
-		display: block;
-		left: 0px;
-		top: 0px;
-		height: 20px;
-		width: 20px;
-		padding: 0;
-		margin: 0;
-		border: 1px solid;
-		background-color: white;
-		font-weight: normal;
-		white-space: nowrap;
-	}
-	menu:hover {
-		background-color: #eef;
-		font-weight: bold;
-	}
-	menu:hover > menu {
-		display: block;
-	}
-	menu > menu {
-		display: none;
-		position: relative;
-		top: -20px;
-		left: 100%;
-		width: 55px;
-	}
-	menu[title]:before {
-		content: attr(title);
-	}
-	menu:not([title]):before {
-		content: '\2630';
-	}
-</style>
