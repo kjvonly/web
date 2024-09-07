f8192d0125c878344f1a082a3b9155e81506b8d7
man4christ <man4christ.65qa8@simplelogin.com>
2024-09-07 15:55:16 -0400

    wiop

--- src/components/memory/memory.svelte
+++ src/components/memory/memory.svelte
@@ -1,19 +1,62 @@
-
 <script lang="ts">
-import type { Buffer } from '../../models/buffer.model';
-import Card from '../card/card.svelte';
+	import { onMount } from 'svelte';
+	import type { Buffer } from '../../models/buffer.model';
+	import Card from '../card/card.svelte';
+	import { chapterService } from '../../api/chapters.service';
 
-export let buffer: Buffer;
-let popup: any;
 
+	export let buffer: Buffer;
+	let popup: any;
+
+	let verse = '';
+	let a = '/api/media/verses/01_Genesis_001_002';
+    let bookNames: any = {}
+
+    function renderBookChapterVerses(){
+        let bcvc = bookNames["bookchapterversecountById"]
+        let bookIds = bcvc.keys().sort()
+        let verses: Array<any> = []
+        bookIds.forEach((bookId: string) => {
+            let chapterIds  = bcvc[bookId].keys().sort()
+            chapterIds.forEach((chapter: string)=> {
+                let verseCount: number = bcvc[bookId][chapter]
+                let bookName = bookNames["booknamesById"][bookId]
+                let range = (n: number) => Array.from(Array(n).keys())
+                range(verseCount).forEach(element => {
+                    let chapterNumber = String(chapter).padStart(2, '0');
+                    let mp3FileName = `${chapter}`
+                   verses.push()
+                });
+            });
+        });
+
+
+    }
+
+	onMount(() => {
+        chapterService.getChapter("booknames").then((data: any) => {
+			bookNames = data;
+            renderBookChapterVerses()
+		});
+
+		const audioElement = document.querySelector('audio');
+        if (audioElement != null){
+            audioElement.addEventListener('ended', () => {
+			audioElement.play();
+		});
+        }
+	});
 </script>
 
-
 <Card bind:buffer bind:popup>
-    <div slot="body" let:bodyHeight>
+	<div slot="body" let:bodyHeight>
+		<button
+			on:click={() => {
+				verse = a;
+			}}>click</button
+		>
 		<div class="kjv-memory" style="max-height: {bodyHeight}px;">
-            <h1>it works</h1>    
-        </div>
-    </div>
-    
+			<audio controls src={verse}></audio>
+		</div>
+	</div>
 </Card>
